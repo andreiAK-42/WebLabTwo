22154960c9895352d0217b2d27b8462ffdcd0fa1
XLEB-YSHEK <andreikorbanl@gmail.com>
2025-10-17 20:30:27 +0700

    Добавлена возможность добавлять таски + логика скрытия таблички "No Tasks"

--- www/src/scripts/main.js
+++ www/src/scripts/main.js
@@ -1,4 +1,41 @@
-const show_task_menu = document.getElementsByClassName(".add-task-inputs-container");
+const show_task_menu = document.getElementsByClassName(".add-task-inputs-container")
+
+const add_task_button = document.querySelector('.add-task-button')
+const tasksList = document.querySelector('.tasks-container')
+var tasks = []
 
 
 
+add_task_button.addEventListener('click', function () {
+    const taskTitle = document.getElementById("inputTaskTitle").value
+    const taskAbout = document.getElementById("inputTaskAbout").value
+
+    const taskCardContainer = document.createElement("div")
+    taskCardContainer.className = "task-card-container"
+    taskCardContainer.innerHTML = `
+        <div class="task-card">
+            <div class="text-task-card">
+                <h1>${taskTitle}</h1>
+                <p>${taskAbout}</p>
+            </div>
+            <div class="delete-button-task-card"></div>
+        </div>
+        <div class="menu-task-container">
+            <div class="share-task-button"></div>
+            <div class="about-task-button"></div>
+            <div class="edit-task-button"></div>
+        </div>
+    `
+    taskCardContainer.id = tasks.length
+
+    tasks.push(tasks.length)
+
+    tasksList.appendChild(taskCardContainer)
+
+    if (tasks && tasks.length == 1) {
+        const element = document.querySelector(".no-task-container")
+        element.style.display = "none"
+        element.style.height = "0"
+    }
+
+});
