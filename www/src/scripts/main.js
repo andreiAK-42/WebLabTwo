d6afbd935427e2633c511d7d1731976ab48fc37e
XLEB-YSHEK <andreikorbanl@gmail.com>
2025-10-17 20:53:30 +0700

    Фикс клика по детям. Триггерился на весь контейнер

--- www/src/scripts/main.js
+++ www/src/scripts/main.js
@@ -29,9 +29,12 @@ add_task_button.addEventListener('click', function () {
     taskCardContainer.id = tasks.length
 
     const taskCard = taskCardContainer.querySelector('.task-card')
-    taskCard.addEventListener('click', function () {
-        const menuTaskContainer = taskCardContainer.querySelector('.menu-task-container')
-        menuTaskContainer.classList.toggle('visible')
+    taskCard.addEventListener('click', function (event) {
+
+        if (event.target == taskCard) {
+            const menuTaskContainer = taskCardContainer.querySelector('.menu-task-container')
+            menuTaskContainer.classList.toggle('visible')
+        }
     })
 
     tasks.push(tasks.length)
